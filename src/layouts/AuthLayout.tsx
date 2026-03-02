c06430d5d4e35afc50a395908951142db8672e2b
Sirio Bambino <bambino.sirio@gmail.com>
2026-03-02 23:05:38 +0000

    feat: Add all necessary auth pages

---
+++ src/layouts/AuthLayout.tsx
@@ -0,0 +1,10 @@
+import { Outlet, ScrollRestoration } from 'react-router-dom';
+
+export const AuthLayout = () => (
+	<div className="flex flex-col min-h-dvh bg-background font-sans antialiased text-foreground">
+		<main className="flex-1 flex flex-col">
+			<Outlet />
+		</main>
+		<ScrollRestoration />
+	</div>
+);
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,7 +1,15 @@
 import { cleanup } from '@testing-library/react';
-import { afterEach } from 'vitest';
+import { afterEach, vi } from 'vitest';
 import '@testing-library/jest-dom/vitest';
 
 afterEach(() => {
 	cleanup();
 });
+
+class ResizeObserverMock {
+	observe() {}
+	unobserve() {}
+	disconnect() {}
+}
+
+vi.stubGlobal('ResizeObserver', ResizeObserverMock);
